d6a52bc2f40540bb75683fe71354fe0e40a6e270
Tadej <bogataj.tadej@gmail.com>
2016-11-23 18:39:41 +0100

    Section timing works as should

--- Prva_stran/Delovni/js/wanalyse.js
+++ Prva_stran/Delovni/js/wanalyse.js
@@ -6,17 +6,27 @@ var time;
 var minutes;
 var seconds;
 
+
+function calculate_time_difference() {
+    how_many_minutes = time.getMinutes() - minutes;
+    how_many_seconds = time.getSeconds() - seconds;
+    if (how_many_seconds < 0) {
+        how_many_seconds = 60 - Math.abs(how_many_seconds);
+    }
+    return ((how_many_minutes * 10) + how_many_seconds);
+}
+
+
 function wanalyse_main() {
     $('li').each(function(i) {
         if ($(this).is('.active')) {
-            console.log(i);
             tmp = i;
         }
     });
 
     //if the function runs the first time it is on the top of the site... meaning it has no sections before
 
-    console.log("TMP: ", tmp);
+    console.log("Section: ", tmp);
     time = new Date();
 
 
@@ -26,20 +36,28 @@ function wanalyse_main() {
         before_section = tmp;
         minutes = time.getMinutes();
         seconds = time.getSeconds();
-        console.log(minutes, seconds);
     } else {
         current_section = tmp;
+
         if (current_section > before_section) console.log("Goind down");
         else if (current_section < before_section) console.log("Going up");
-        before_section = current_section;
-        console.log("getminutes", time.getMinutes());
-        console.log("minutes", minutes);
-        console.log("getseconds", time.getSeconds());
-        console.log("seconds", seconds);
-        time_spent_on_section = (time.getMinutes() - minutes) * 10 + (time.getSeconds() - seconds);
+
+
+
+        time_spent_on_section = calculate_time_difference();
+
         console.log("Time spent on section: ", time_spent_on_section);
+
+        //if the user is less then 4 seconds on an individual section it is not counted as a "section view" cuz he just passes by
+        if (time_spent_on_section < 4) {
+            console.log("Dont't bother, right?");
+        } else {
+            console.log("Wow, youre interested");
+        }
+        before_section = current_section;
         minutes = time.getMinutes();
         seconds = time.getSeconds();
+
     }
 
 }
