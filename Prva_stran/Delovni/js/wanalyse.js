31116a093e1fc08247a31bf5e9126d77d82004bc
Tadej <bogataj.tadej@gmail.com>
2016-11-23 18:08:38 +0100

    Getting stuff done

--- Prva_stran/Delovni/js/wanalyse.js
+++ Prva_stran/Delovni/js/wanalyse.js
@@ -2,7 +2,7 @@ var first_time = true;
 var before_section;
 var current_section;
 var tmp;
-var time = new Date();
+var time;
 var minutes;
 var seconds;
 
@@ -17,6 +17,8 @@ function wanalyse_main() {
     //if the function runs the first time it is on the top of the site... meaning it has no sections before
 
     console.log("TMP: ", tmp);
+    time = new Date();
+
 
     if (first_time == true) {
         first_time = false;
@@ -24,11 +26,20 @@ function wanalyse_main() {
         before_section = tmp;
         minutes = time.getMinutes();
         seconds = time.getSeconds();
+        console.log(minutes, seconds);
     } else {
         current_section = tmp;
         if (current_section > before_section) console.log("Goind down");
         else if (current_section < before_section) console.log("Going up");
         before_section = current_section;
+        console.log("getminutes", time.getMinutes());
+        console.log("minutes", minutes);
+        console.log("getseconds", time.getSeconds());
+        console.log("seconds", seconds);
+        time_spent_on_section = (time.getMinutes() - minutes) * 10 + (time.getSeconds() - seconds);
+        console.log("Time spent on section: ", time_spent_on_section);
+        minutes = time.getMinutes();
+        seconds = time.getSeconds();
     }
 
 }
